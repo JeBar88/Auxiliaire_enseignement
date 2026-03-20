a2e3d6564eff8f819d7f37cb32975f0734cfea50
Jeremie Barde <jeremiebarde@Ordinateur-portable-de-Jeremie.local>
2026-03-20 10:20:30 -0400

    Update to code

--- ACT-3000/Code/Discret_bivariee_base.R
+++ ACT-3000/Code/Discret_bivariee_base.R
@@ -2,15 +2,59 @@
 ## Exemple de base pour une v.a discrète bivariée
 ## Jérémie Barde
 
+#### Package utile ####
+library(actuar)
 #### Fonction utile ####
-repart <- function(x, y, fxy){
+repart_2d <- function(x, y, fxy){
   f <- function(i, j) sum(fxy[0:i + 1, 0:j + 1])
   outer(x, y, Vectorize(f))
 }
 
-ds <- function(k, f){
+repart_3d <- function(k, d, f) {
+  res <- array(0, dim(f))
+  
+  for (i in k) {
+    for (j in k) {
+      for (l in k) {
+        res[i + 1, j + 1, l + 1] <- sum(f[0:i + 1, 0:j + 1, 0:l + 1])
+      }
+    }
+  }
+  res
+}
+
+# Explication: On calule les sommmes cumulatives en fixant les dimensions, comme 
+# apply mélange les dimensions on utilise aperm pour les remettres dans le bonne autre.
+repart <- function(f) {
+  d <- length(dim(f))
+  for (i in 1:d) {
+    f <- apply(f, (1:d)[-i], cumsum) |> aperm(order(c(i, (1:d)[-i])))
+  }
+  f
+}
+
+ds_2d <- function(k, f){
   fs <- function(s) sum(sapply(0:s, function(i) f[i + 1, s - i + 1]))
   sapply(k, fs)
+}
+
+ds_3d <- function(k, f){
+  fs <- function(s){
+    res <- 0
+    for (k1 in 0:s) {
+      for (k2 in 0:(s - k1)) {
+        res <- res + f[k1 + 1, k2 + 1, (s - k1 - k2) + 1]
+      }
+    }
+    res
+  }
+  sapply(k, fs)
+}
+
+ds <- function(k, d, f) {
+  index <- arrayInd(seq_along(f), dim(f)) - 1
+  sums <- rowSums(index)
+  sapply(k, function(s) sum(f[sums == s]))
 }
 
 #### Exemple 1 -- Loi discrete ####
@@ -36,17 +80,128 @@ cov <- Em12 - Em1*Em2
 pp <- cov/sqrt(Vm1*Vm2)
 
 ## Fonction de repartition
-Fm12 <- repart(k, k, fm12)
+Fm12 <- repart_2d(k, k, fm12)
+# Fm12 <- repart_nd(fm12)
 
 ## V.a. S = M1 + M2
-kmax <- max(k)*2 + 1
-fm12_0 <- matrix(0, ncol=kmax, nrow=kmax)
+kmax <- max(k)*2
+fm12_0 <- matrix(0, ncol=kmax + 1, nrow=kmax + 1)
 fm12_0[k + 1, k + 1] <- fm12
 
-fs <- ds(0:4, fm12_0)
-c("ES_test"=sum(0:4*fs), "ES"=Em1 + Em2)
+fs <- ds_2d(0:kmax, fm12_0)
+# ou
+fs <- ds(0:kmax, d, fm12)
+c("ES_test"=sum(0:kmax*fs), "ES"=Em1 + Em2)
+
+#### Exemple 2 -- Loi discrete dimension d ####
+fm123 <- array(c(0.083, 0.031, 0.054, 0.054, 0.050, 0.005, 0.035, 
+                 0.047, 0.051, 0.117, 0.044, 0.011, 0.114, 0.032,
+                 0.025, 0.088, 0.028, 0.013, 0.005, 0.003, 0.009,
+                 0.014, 0.001, 0.019, 0.024, 0.012, 0.031), dim=rep(3, 3))
+d <- 3
+k <- 0:2
+
+## Calculer les marginales
+fmi <- sapply(1:d, function(i) apply(fm123, i, sum))
+
+comb <- combn(d, 2)
+fmij <- mapply(function(i, j) apply(fm123, c(i, j), sum), comb[1 ,], comb[2, ], SIMPLIFY = FALSE)
+names(fmij) <- paste0('fm', comb[1, ], comb[2, ])
 
 
+## Calcule EMi et VarMi
+Emi <- apply(fmi, 2, function(f) sum(k*f))
+Em123 <- sum(Reduce("%o%", rep(list(k), d))*fm123)
+
+Vmi <- sapply(1:d, function(i) sum(k^2*fmi[, i]) - Emi[i]^2)
+
+## Coveriance et correlation de Pearson
+covmij <- sapply(1:ncol(comb), function(i) sum(outer(k, k)*fmij[[i]]) - prod(Emi[comb[, i]]))
+names(covmij) <- paste0('covm', comb[1, ], comb[2, ])
+
+ppmij <- sapply(1:d, function(i) covmij[i]/sqrt(prod(Vmi[comb[, i]])))
+names(ppmij) <- paste0('pp', comb[1, ], comb[2, ])
+
+
+## Fonction de repartition
+Fm123 <- repart_3d(k, d, fm123)
+# Fm123 <- repart_nd(fm123)
+
+## V.a. S = M1 + M2 + M3
+kmax <- max(k)*3
+fm123_0 <- array(0, dim = rep(kmax + 1, d))
+fm123_0[k + 1, k + 1, k + 1] <- fm123
+
+k2 <- 0:kmax
+fs <- ds_3d(k2, fm123_0)
+# ou
+fs <- ds(k2, d, fm123)
+c("Es_test"=sum(k2*fs), "Es"=sum(Emi))
+
+#### Exemple 3 - dimension 5 ####
+# pmf d'une Bernouilli CGMR avec q0 <- 0.1 et qt <- c(0.5, 0.25, 0.15, 0.25, 0.20)
+fm5 <- array(c(0.172125, 0.172125, 0.057375, 0.057375,
+              0.030375, 0.030375, 0.010125, 0.010125,
+              0.057375, 0.057375, 0.019125, 0.019125,
+              0.010125, 0.010125, 0.003375, 0.003375,
+              0.04303125, 0.04303125, 0.01434375, 0.01434375,
+              0.00759375, 0.00759375, 0.00253125, 0.00253125,
+              0.01434375, 0.01434375, 0.00478125, 0.00478125,
+              0.00253125, 0.00253125, 0.00084375, 0.10084375), dim = rep(2, 5))
+d <- 5
+k <- 0:1
+
+## Calculer les marginales
+# Marginale univariée
+fmi <- sapply(1:d, function(i) apply(fm5, i, sum))
+
+# Marginale bivariée
+comb <- combn(d, 2)
+fmij <- mapply(function(i, j) apply(fm5, c(i, j), sum), comb[1 ,], comb[2, ], SIMPLIFY = FALSE)
+names(fmij) <- paste0('fm', comb[1, ], comb[2, ])
+sapply(fmij, sum)
+
+# Marginale trivariée
+comb3 <- combn(d, 3)
+fmijl <- mapply(function(i, j, l) apply(fm5, c(i, j, l), sum), comb3[1 ,], comb3[2, ], comb3[3, ], SIMPLIFY = FALSE)
+names(fmijl) <- paste0('fm', comb3[1, ], comb3[2, ], comb3[3, ])
+sapply(fmijl, sum)
+
+# Marginale quadrivariée
+comb4 <- combn(d, 4)
+fmijlh <- mapply(function(i, j, l, h) apply(fm5, c(i, j, l), sum), comb4[1 ,], comb4[2, ], comb4[3, ], comb4[4, ], SIMPLIFY = FALSE)
+names(fmijlh) <- paste0('fm', comb4[1, ], comb4[2, ], comb4[3, ], comb4[4, ])
+sapply(fmijlh, sum)
+
+
+## Calcule EMi et VarMi
+Emi <- apply(fmi, 2, function(f) sum(k*f))
+Em12345 <- sum(Reduce("%o%", rep(list(k), d))*fm5)
+
+Vmi <- sapply(1:d, function(i) sum(k^2*fmi[, i]) - Emi[i]^2)
+
+## Coveriance et correlation de Pearson
+covmij <- sapply(1:ncol(comb), function(i) sum(outer(k, k)*fmij[[i]]) - prod(Emi[comb[, i]]))
+names(covmij) <- paste0('covm', comb[1, ], comb[2, ])
+
+
+
+ppmij <- sapply(1:ncol(comb), function(i) covmij[i]/sqrt(prod(Vmi[comb[, i]])))
+names(ppmij) <- paste0('pp', comb[1, ], comb[2, ])
+
+
+## Fonction de repartition
+Fm5 <- repart(fm5)
+
+## V.a. S = M1 + M2 + M3
+kmax <- max(k)*d
+fm5_0 <- array(0, dim = rep(kmax + 1, d))
+fm5_0[k + 1, k + 1, k + 1, k + 1, k + 1] <- fm5
+
+k2 <- 0:kmax
+fs <- ds(k2, d, fm5)
+sum(fs)
+c("Es_test"=sum(k2*fs), "Es"=sum(Emi))
 
 
 
--- ACT-3000/Code/PoTeicher_comp_simul.R
+++ ACT-3000/Code/PoTeicher_comp_simul.R
@@ -2,86 +2,127 @@
 ## Loi Poisson Teicher composée : simulation
 ## Jérémie Barde
 
-##### Packages #####
+#### Packages ####
 library(actuar)
 
-##### Fonction utiles #####
-dPoTei <- function(m1, m2, lam1, lam2, a){
-  f <- matrix(numeric(length(m1) * length(m2)), ncol = length(m2))
-  
-  for (i in m1){
-    for (j in m2){
-      
-      k <- 0:min(i, j)
-      
-      f[i + 1, j + 1] <-  sum(dpois(k, a) * dpois(i - k, lam1 - a) * dpois(j - k, lam2 - a))
-    }
+#### Fonction utiles ####
+dPoTei <- function(k1, k2, lam1, lam2, a0){
+  f <- function(i, j){
+    k <- 0:min(i, j)
+    sum(dpois(k, a0) * dpois(i - k, lam1 - a0) * dpois(j - k, lam2 - a0))
   }
-  f
+  outer(k1, k2, Vectorize(f))
 }
 
-### Poisson Teicher composée simulation ###
-n <- 1e5
+#### Poisson Teicher composée simulation ####
+m <- 1e6
+k <- 0:20
 lam <- c(2, 3)
-a <- 1
-m1 <- 0:20
-m2 <- m1
-k <- 0.95
+a0 <- 1.5
+a <- 2
+b <- c(0.1, 10)
+u <- 0.95
 
-### Densité de la loi Teicher
-fm12 <- dPoTei(m1, m2, lam[1], lam[2], a)
+## Densité de la loi Teicher
+fm12 <- dPoTei(k, k, lam[1], lam[2], a0)
 
-### Simulation de la fréquence
-
-M1 <- rpois(1, lam[1])
-fm2c1 <- fm12[M1 + 1, ]/dpois(M1, lam[1])
-Fm2c1 <- cumsum(fm2c1)
-M2 <- m2[min(which(Fm2c1 >= runif(1)))]
-c(M1, M2)
-
-## Façon 1
+## Simulation de la fréquence
+## Façon 1 -- Intuitive
 simul_freq <- function(){
   M1 <- rpois(1, lam[1])
   fm2c1 <- fm12[M1 + 1, ]/dpois(M1, lam[1])
   Fm2c1 <- cumsum(fm2c1)
-  M2 <- m2[min(which(Fm2c1 >= runif(1)))]
+  M2 <- k[min(which(Fm2c1 >= runif(1)))]
   c(M1, M2)
 }
-M12 <- t(replicate(n, simul_freq()))
+U12 <- t(replicate(m, simul_freq()))
 
-# ## Façon 2
-# U1 <- rpois(n, lam[1])
-# fm2c1 <- sapply(U1, function(i) fm12[i + 1, ]/dpois(i, lam[1]))
-# Fm2c1 <- apply(fm2c1, 2, cumsum)
-# U2 <- apply(Fm2c1, 2, function(i) m2[min(which(i >= runif(1)))])
-# U12 <- cbind(U1, U2)
+## Façon 1.5 -- Sans fonction
+U1 <- rpois(m, lam[1])
+fm2c1 <- sapply(U1, function(i) fm12[i + 1, ]/dpois(i, lam[1]))
+Fm2c1 <- apply(fm2c1, 2, cumsum)
+U2 <- apply(Fm2c1, 2, function(i) k[min(which(i >= runif(1)))])
+U12 <- cbind(U1, U2)
 
-### Simulation de la sévérité
-X1 <- sapply(M12[, 1], function(i) sum(rgamma(i, 2, 0.1)))
-X2 <- sapply(M12[, 2], function(i) sum(rpareto(i, 2, 10)))
+## Façon 2 -- Simuler avec la fmp conjoite
+comb <- expand.grid(k, k) |> as.matrix()
+fm <- as.vector(fm12)
+index <- sample(seq_along(fm), m, replace = TRUE, prob = fm)
+M12 <- comb[index, ]
+
+## Verif
+CovM12 <- cov(M12)
+cbind("a_test"=CovM12[1, 2], a0)
+cbind("Vm1_test"=CovM12[1, 1], "Vm1"=lam[1],
+      "Vm2_test"=CovM12[2, 2], "Vm2"=lam[2])
+
+## Simulation de la sévérité
+X1 <- sapply(M12[, 1], function(i) sum(rgamma(i, a, b[1])))
+X2 <- sapply(M12[, 2], function(i) sum(rpareto(i, a, b[2])))
 X12 <- cbind(X1, X2)
 
-### Simualtion de la somme X1 + X2
+## Simualtion de la somme X1 + X2
 S <- rowSums(X12)
 
-## Covariance
-covX12 <- mean(X1 * X2) - mean(X1)*mean(X2)
+# Covariance
+CovX12 <- mean(X1 * X2) - mean(X1)*mean(X2)
+cbind("CovX12_test"=CovX12, "CovX12"=a0*a/b[1]*b[2]/(a - 1))
 
-## Correlation de pearson
-rho <- covX12/(sd(X1) * sd(X2))
+# Correlation de pearson
+rho <- CovX12/(sd(X1) * sd(X2))
 
-## Espérance et variance de S
-espS <- mean(S)
-varS <- var(S)  
+# Espérance et variance
+ES <- mean(S)
+VS <- var(S)  
 
-## VaR de S
-VaRS <- quantile(S, k)
+# VaR
+VaRS <- quantile(S, u)
 
-## TVaR de S
-TVaRS1 <- mean(S[S > VaRS])
-TVaRS2 <- VaRS + 1/(1 - k)*mean(pmax(S - VaRS, 0))
+# TVaR
+TVaRS1 <- mean(S[S > VaRS]) # Si continue
+TVaRS2 <- VaRS + 1/(1 - u)*mean(pmax(S - VaRS, 0))
 cbind(TVaRS1, TVaRS2)  
   
+#### Loi discrete en dimension d simulation ####
+fm <- array(c(0.083, 0.031, 0.054, 0.054, 0.050, 0.005, 0.035,
+                 0.047, 0.051, 0.117, 0.044, 0.011, 0.114, 0.032,
+                 0.025, 0.088, 0.028, 0.013, 0.005, 0.003, 0.009,
+                 0.014, 0.001, 0.019, 0.024, 0.012, 0.031), dim=rep(3, 3))
+
+# fm <- array(c(0.172125, 0.172125, 0.057375, 0.057375,
+#                0.030375, 0.030375, 0.010125, 0.010125,
+#                0.057375, 0.057375, 0.019125, 0.019125,
+#                0.010125, 0.010125, 0.003375, 0.003375,
+#                0.04303125, 0.04303125, 0.01434375, 0.01434375,
+#                0.00759375, 0.00759375, 0.00253125, 0.00253125,
+#                0.01434375, 0.01434375, 0.00478125, 0.00478125,
+#                0.00253125, 0.00253125, 0.00084375, 0.10084375), dim = rep(2, 5))
+
+m <- 1e6
+d <- length(dim(fm))
+k <- 0:(dim(fm)[1] - 1)
+
+## Marginales
+fmi <- sapply(1:d, function(i) apply(fm, i, sum))
+Emi <- apply(fmi, 2, function(f) sum(k*f))
+Es <- sum(Emi)
+
+## Simulation du Vecteur M
+comb <- arrayInd(seq_along(fm), dim(fm)) - 1
+fm_vec <- as.vector(fm)
+index <- sample(seq_along(fm_vec), m, replace = TRUE, prob = fm)
+M <- comb[index, ]
+
+# Repartion
+# value <- c(0, 1, 1, 0, 1)
+# mean(apply(M, 1, function(i) all(i <= value)))
+
+## Simualtion de la v.a. S
+S <- rowSums(M)
+ES <- mean(S)
+cbind(Es, ES)
+
+
 
 
 
