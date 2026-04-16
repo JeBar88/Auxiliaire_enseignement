1d51194c11c8589718ab89efab20b1307808e1a3
Jeremie Barde <jebar88@ulaval.ca>
2026-04-16 19:07:57 -0400

    Fixe code

--- ACT-3000/Examen pratique/2016/Partiel informatique/EXPart2016info5.R
+++ ACT-3000/Examen pratique/2016/Partiel informatique/EXPart2016info5.R
@@ -1,5 +1,5 @@
 ### 5 2016 info partiel
-nfft <- 2^16
+nfft <- 2^10
 k <- 0:(nfft - 1)
 n <- 10
 q <- c(0.2, 0.3)
@@ -16,12 +16,13 @@ Es <- sum(Exi)
 fb <- sapply(q, function(i) dbinom(k, n, i))
 fbt <- mvfft(fb)
 
-fgp_theta <- function(t1, t2){
+fgp_thetai <- function(t1, t2){
   ((1 - gam)/((1 - (1 - gam)/r*t1)*(1 - (1 - gam)/r*t2)-gam))^r
 }
 
-fst <- fgp_theta(lam[1]*(fbt[, 1] - 1), lam[2]*(fbt[, 2] - 1))
+fst <- fgp_thetai(lam[1]*(fbt[, 1] - 1), lam[2]*(fbt[, 2] - 1))
 fs <- Re(fft(fst, TRUE))/nfft
+sum(fs*k)
 
 Fs <- cumsum(fs)
 Fs[c(30, 35, 40) + 1]
